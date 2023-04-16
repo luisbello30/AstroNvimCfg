9f1bfcd963ea4dea3b03631dbd71f636c7488df4
Luis Bello <luisbello25@icloud.com>
2023-04-16 05:17:59 +0200

    toogle boolean plugins

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,27 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "gerazov/toggle-bool.nvim",
+    event = "BufRead",
+    config = function()
+      require("toggle-bool").setup {
+        mapping = "<leader>tt",
+        additional_toggles = {
+          Yes = 'No',
+          On = 'Off',
+          ["0"] = "1",
+          Enable = 'Disable',
+          Enabled = 'Disabled',
+          First = 'Last',
+          Before = 'After',
+          Persistent = 'Ephemeral',
+          Internal = 'External',
+          Ingress = 'Egress',
+          Allow = 'Deny',
+          All = 'None',
+        },
+      }
+    end
+  }
 }
